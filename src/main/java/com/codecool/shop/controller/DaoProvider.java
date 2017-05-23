c863987a68de6a7ba4f3f2fe74af647b449ec21c
keligeri <kel.gergo@gmail.com>
2017-05-23 14:24:46 +0200

    Added javaDoc to controller folder's files

--- src/main/java/com/codecool/shop/controller/DaoProvider.java
+++ src/main/java/com/codecool/shop/controller/DaoProvider.java
@@ -4,7 +4,12 @@ import com.codecool.shop.dao.*;
 import com.codecool.shop.dao.implementation.*;
 
 /**
- * Created by keli on 2017.05.18..
+ *
+ * The {@link DaoProvider} class responsible to decide, which object have to return.
+ * <p>
+ * If the only one public method <code>setupDaoJdbc</code> parameter is <code>true</code>,
+ * the class setup the instances from DaoJdbc instances, else setup with DaoMem instances.
+ *
  */
 public class DaoProvider {
     public static ProductDao productDao;
@@ -13,11 +18,18 @@ public class DaoProvider {
     public static ProductCategoryDao productCategoryDao;
     public static SupplierDao supplierDao;
 
+    /**
+     * Invoke the setup method, depend on the isDb parameter.
+     * @param isDb if is <code>true</> setup variables to JDBC instances, else setup with Mem instances.
+     */
     public static void setup(boolean isDb) {
         if (!isDb) setupDaoMem();
         else setupDaoJdbc();
     }
 
+    /**
+     * Assign the variables with DaoJdbc instances.
+     */
     private static void setupDaoJdbc(){
         productDao = new ProductDaoImplJdbc();
         lineItemDao = new LineItemDaoImplJdbc();
@@ -26,6 +38,9 @@ public class DaoProvider {
         supplierDao = new SupplierDaoJdbc();
     }
 
+    /**
+     * Assign the variables with DaoMem instances.
+     */
     private static void setupDaoMem(){
         productDao = ProductDaoMem.getInstance();
         lineItemDao = LineItemDaoMem.getInstance();
--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -1,11 +1,10 @@
 package com.codecool.shop.controller;
-
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoImplJdbc;
+
 import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.implementation.*;
 import com.codecool.shop.model.ShoppingCart;
 import spark.Request;
 import spark.Response;
@@ -15,18 +14,47 @@ import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The {@link ProductController} class responsible to transmit parameters
+ * to Thymeleaf {@link ModelAndView} template.
+ */
 public class ProductController {
     private static ProductDao productDataStore;
     private static ProductCategoryDao productCategoryDataStore;
     private static SupplierDao supplierDataStore;
 
+    /**
+     * Check the {@link DaoProvider} instances and if the
+     *  <ul>
+     *      <li>productDataStore</li>
+     *      <li>productCategoryDataStore</li>
+     *      <li>supplierDataStore</li>
+     *  </ul>
+     *   If the above variables values is null setup with the DaoProvider instances.
+     */
     private static void checkAndSetupStaticVar(){
         if (productDataStore == null) productDataStore = DaoProvider.productDao;
         if (productCategoryDataStore == null) productCategoryDataStore = DaoProvider.productCategoryDao;
         if (supplierDataStore == null) supplierDataStore = DaoProvider.supplierDao;
     }
 
-    public static ModelAndView renderProducts(Request req, Response res) throws SQLException {
+    /**
+     * Render all products from the database
+     * <p>
+     * Render all product from the product table. Store the product's information in in a HashMap,
+     * with the following key
+     * <ul>
+     *     <li>categories ({@link ProductDaoImplJdbc})</li>
+     *     <li>suppliers ({@link SupplierDaoJdbc})</li>
+     *     <li>products ({@link ProductDaoImplJdbc})</li>
+     *     <li>shoppingListSize ({@link ShoppingCart})</li>
+     * </ul>
+     *
+     * @param req {@link Request} object from the request
+     * @param res {@link Response} object from the response
+     * @return a new {@link ModelAndView} instance, with product/index viewName
+     */
+    public static ModelAndView renderProducts(Request req, Response res) {
         checkAndSetupStaticVar();
         Map params = new HashMap<>();
         ShoppingCart currentSession = req.session().attribute("shoppingCart");
@@ -38,7 +66,23 @@ public class ProductController {
         return new ModelAndView(params, "product/index");
     }
 
-    public static ModelAndView renderByCategory(Request req, Response res) throws SQLException {
+    /**
+     * Render all products by the given category
+     * <p>
+     * Render all product from the product table by the given category.
+     * Store the product's information in in a HashMap, with the following key
+     * <ul>
+     *     <li>categories ({@link ProductDaoImplJdbc})</li>
+     *     <li>suppliers ({@link SupplierDaoJdbc})</li>
+     *     <li>products ({@link ProductDaoImplJdbc})</li>
+     *     <li>shoppingListSize ({@link ShoppingCart})</li>
+     * </ul>
+     *
+     * @param req {@link Request} object from the request
+     * @param res {@link Response} object from the response
+     * @return a new {@link ModelAndView} instance, with product/index viewName
+     */
+    public static ModelAndView renderByCategory(Request req, Response res) {
         checkAndSetupStaticVar();
         int searchedId = Integer.parseInt(req.params(":id"));
         ShoppingCart currentSession = req.session().attribute("shoppingCart");
@@ -51,7 +95,23 @@ public class ProductController {
         return new ModelAndView(params, "product/index");
     }
 
-    public static ModelAndView renderBySupplier(Request req, Response res) throws SQLException {
+    /**
+     * Render all products by the given supplier
+     * <p>
+     * Render all product from the product table by the given supplier.
+     * Store the product's information in in a HashMap, with the following key
+     * <ul>
+     *     <li>categories ({@link ProductDaoImplJdbc})</li>
+     *     <li>suppliers ({@link SupplierDaoJdbc})</li>
+     *     <li>products ({@link ProductDaoImplJdbc})</li>
+     *     <li>shoppingListSize ({@link ShoppingCart})</li>
+     * </ul>
+     *
+     * @param req {@link Request} object from the request
+     * @param res {@link Response} object from the response
+     * @return a new {@link ModelAndView} instance, with product/index viewName
+     */
+    public static ModelAndView renderBySupplier(Request req, Response res) {
         checkAndSetupStaticVar();
         int searchedId = Integer.parseInt(req.params(":id"));
         Map params = new HashMap<>();
@@ -64,6 +124,21 @@ public class ProductController {
         return new ModelAndView(params, "product/index");
     }
 
+    /**
+     * Render all line item ({@link com.codecool.shop.model.LineItem}) with a new route
+     * <p>
+     * Render all line item from the database.
+     * Store the items's information in in a HashMap, with the following key
+     * <ul>
+     *     <li>totalprice ({@link ProductDaoImplJdbc})</li>
+     *     <li>currency ({@link SupplierDaoJdbc})</li>
+     * </ul>
+     *
+     * @param req {@link Request} object from the request
+     * @param res {@link Response} object from the response
+     * @param shoppingCart the current ShoppingCart {@link ShoppingCart} instance
+     * @return a new {@link ModelAndView} instance, with product/cart viewName
+     */
     public static ModelAndView renderToCart(Request req, Response res, ShoppingCart shoppingCart) {
         checkAndSetupStaticVar();
         Map params = new HashMap<>();
@@ -78,6 +153,14 @@ public class ProductController {
         return new ModelAndView(params, "product/cart");
     }
 
+
+    /**
+     * Render to the checkout route, without data.
+     *
+     * @param req {@link Request} object from the request
+     * @param res {@link Response} object from the response
+     * @return a new {@link ModelAndView} instance, with product/cart viewName
+     */
     public static ModelAndView renderToCheckout(Request req, Response res) {
         checkAndSetupStaticVar();
         Map params = new HashMap<>();
